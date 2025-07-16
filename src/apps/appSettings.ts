05dc793961a5d3fc67e5765b072683b5fd19c849
ItsRiprod <riprodgames@gmail.com>
2025-07-16 14:13:31 -0400

    Updated dependsOn array

--- src/apps/appSettings.ts
+++ src/apps/appSettings.ts
@@ -25,17 +25,34 @@ export type CommonSetting = {
    * If no version is specified, the task will use the apps version
    * If the version specified is different from the version saved, it will be overwritten on initialization
    * Otherwise, the setting will persist from older versions
-   * @since 0.11.13
+   * @since 0.11.14
    */
   version?: string;
   /**
    * An array of setting IDs that this setting depends on being enabled to be interactable
-   * If any of the settings in this array are disabled, this setting will also be disabled
-   * Additionally, if the setting is type BOOLEAN and the value is false, this setting will also be disabled
-   * If the setting is type STRING or type MULTISELECT and the value is an empty string or array, this setting will also be disabled
+   * 
+   * If isValue and isNot are not provided, the following logic is applied:
+   * - If any of the settings in this array are disabled, this setting will also be disabled
+   * - If the setting is type BOOLEAN and the value is false, this setting will also be disabled
+   * - If the setting is type STRING or type MULTISELECT and the value is an empty string or array, this setting will also be disabled
    * @since 0.11.14
    */
-  dependsOn?: string[]
+  dependsOn?: {
+    /**
+     * The ID of the setting that this setting depends on
+     */
+    settingId: string
+    /**
+     * If the setting is or includes one of these values, this setting will be enabled.
+     * This takes higher precedence than the setting being disabled
+     */
+    isValue?: string | number;
+    /**
+     * If the setting is or includes one of these values, this setting will be disabled.
+     * This takes lower precedence than the setting being enabled
+     */
+    isNot?: string | number | boolean;
+  }[]
   id?: string
   label: string;
   value: unknown
